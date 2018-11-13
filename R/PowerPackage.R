b813c73cc5d5a9b617c841953e56c4e0ab0ba030
MichaelSu <michaelsuaustralia@hotmail.com>
2018-11-13 16:48:49 +1100

    Initial

---
+++ R/PowerPackage.R
@@ -0,0 +1,63 @@
+#' Square Calculation
+#'
+#' @param x numeric
+#'
+#' @return numeric
+#'
+#' @export
+#'
+#' @examples
+#' square(2)
+square<-function(x){
+            x^2
+}
+
+
+#' Cube Calculation
+#'
+#' @param x numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' cube(2)
+cube<-function(x){
+  x^4
+}
+
+
+
+
+#' power with n calculation
+#'
+#' @param x numeric
+#' @param n numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' npower(2,5)
+npower<-function(x,n){
+  x^n
+}
+
+
+#' @keywords internal
+"_PACKAGE"
+#' Powerpackge: This is my frist package
+#'
+#'This package was created to as an exercise to learn creating R packages
+#'
+#' @docType package
+#' @name mypackage
+NULL
+
+#sampledata<-c(1,2,3,4,5)
+#use_data(sampledata)
+
+
+
+
+
